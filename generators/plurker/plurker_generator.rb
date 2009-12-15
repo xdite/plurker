f37ffb9caa438217ef474cb82252f6357eff0b3a
xdite <xdite@pixnet.tw>
2009-12-15 17:35:06 +0800

    fix generator class

--- generators/plurker/plurker_generator.rb
+++ generators/plurker/plurker_generator.rb
@@ -1,4 +1,4 @@
-class PlurkGenerator < Rails::Generator::Base
+class PlurkerGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.file 'config/plurker.yml', 'config/plurker.yml'
